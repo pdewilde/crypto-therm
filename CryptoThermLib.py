1519df4d03a6edf60c0b407a16f0e9a9276bc0ea
pdewilde <parkerdewilde@gmail.com>
2020-11-13 20:20:33 -0800

    Basic LCD printing

---
+++ CryptoThermLib.py
@@ -0,0 +1,5 @@
+def cToF(c: float) -> float:
+  return c * (9.0/5) + 32
+
+def fToC(f: float) -> float:
+  return (f - 32) * (5.0/9)
---
+++ LcdDisplay.py
@@ -0,0 +1,15 @@
+from RPi import GPIO
+from RPLCD import CharLCD
+from CryptoThermLib import cToF, fToC
+GPIO.cleanup()
+lcd = CharLCD(numbering_mode=GPIO.BOARD, cols=16, rows=2, pin_rs=37, pin_e=35, pins_data=[33, 31, 29, 23])
+lcd.clear()
+
+# 0000000000000000
+# CUR: 70F | State
+# SET: 72F |   ON    # or OFF
+def getTempAndStatusStr(curTemp: float, setTemp: float, unit: str, status: str) -> string:
+    return "CUR:{:3.0f}{:1} | StateSET:{:3.0f}{:1} |{:>6}".format(curTemp, unit, setTemp, unit, status)
+
+GPIO.cleanup()
+
